5201dae7e7d76d9a07d36b1fff1d9c1318443325
oocheol <oocheol@naver.com>
2022-01-15 01:07:37 +0900

    22/01/15

--- src/Variables.java
+++ src/Variables.java
@@ -2,6 +2,21 @@ public class Variables {
     public static void main(String[] args){
         int age ;
         age = 13;
+        System.out.print("int ");
         System.out.println(age+1);
+
+        long big = 123154263464636346L ;
+        System.out.print("long ");
+        System.out.println(big);
+
+        double float_num = 3.14 ;
+        System.out.print("double ");
+        System.out.println(float_num);
+
+        String str = "Codeit!";
+        System.out.print("String ");
+        System.out.println(str);
+
+
     }
 }
