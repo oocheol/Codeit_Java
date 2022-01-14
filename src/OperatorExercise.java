1fa2ce3bcc9a26d4ba1a53f4eae8d81d786372c1
oocheol <oocheol@naver.com>
2022-01-15 01:52:35 +0900

    22/01/15

---
+++ src/OperatorExercise.java
@@ -0,0 +1,13 @@
+public class OperatorExercise {
+    public static void main(String[] args) {
+        // 각 과목 점수
+        int koreanScore = 86;
+        int mathScore = 94;
+        int scienceScore = 87;
+        int computerScore = 100;
+
+        // 평균 점수
+        int total = koreanScore + mathScore + scienceScore + computerScore;
+        System.out.println(total / 4.0);
+    }
+}
