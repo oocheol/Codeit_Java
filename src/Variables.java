82e87f47f7f98495305eb6db1f2d800c798adcce
oocheol <oocheol@naver.com>
2022-01-15 01:10:19 +0900

    22/01/15

--- src/Variables.java
+++ src/Variables.java
@@ -5,6 +5,11 @@ public class Variables {
         System.out.print("int ");
         System.out.println(age+1);
 
+        boolean myBoolean = true;
+        myBoolean = false;
+        System.out.print("boolean ");
+        System.out.println(myBoolean);
+
         long big = 123154263464636346L ;
         System.out.print("long ");
         System.out.println(big);
