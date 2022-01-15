e880e371fe4e6659db3f05cbba10b65480a59fcb
oocheol <oocheol@naver.com>
2022-01-15 22:01:44 +0900

    22/01/15

---
+++ src/IfElse.java
@@ -0,0 +1,18 @@
+public class IfElse {
+    public static void main(String[] args){
+        int temp = 11;
+
+        if (temp <= 5){
+            System.out.println("자켓을 입으세요");
+        }
+        else if (temp <= 10){
+            System.out.println("자켓을 입지마세요");
+        }
+
+        else{
+            System.out.println("반팔을 입으세요");
+        }
+
+
+    }
+}
