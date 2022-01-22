d6557b1593c539c0b9395e264462d9e2296857c1
oocheol <oocheol@naver.com>
2022-01-22 13:30:18 +0900

    22/01/22

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -63,15 +63,29 @@ public class BankAccount {
     // 두 번째 파라미터 : 이체할 금액(정수)
     // 리턴 : 성공여부(불린)
     public boolean transfer(Person to, int amount) {
-        transfer(to.getAccount(), amount);
-        return true;
+        if (amount < 0 && amount > balance) {
+            System.out.println("false - from: " + owner.getName() + ", to: " + to.getName() + ", amount: " + amount + ", balance: " + getBalance());
+            return false;
+        } else {
+            setBalance(getBalance() - amount);
+            to.getAccount().setBalance(to.getAccount().getBalance() + amount);
+            System.out.println("true - from: " + owner.getName() + ", to: " + to.getName() + ", amount: " + amount + ", balance: " + getBalance());
+            return true;
+    }
     }
 
     // 첫 번째 파라미터: 받는 사람의 계정 (BankAccount)
     // 두 번째 파라미터: 이체할 금액 (정수)
     // 리턴 : 성공여부 (불린)
     public boolean transfer(BankAccount to, int amount) {
-        to.setBalance(to.getBalance() + amount);
+        if (amount < 0 && amount > balance) {
+            System.out.println("false - from: " + owner.getName() + ", to: " + to.getOwner().getName() + ", amount: " + amount + ", balance: " + getBalance());
+            return false;
+        } else {
+            setBalance(getBalance() - amount);
+            to.setBalance(to.getBalance() + amount);
+            System.out.println("true - from: " + owner.getName() + ", to: " + to.getOwner().getName() + ", amount: " + amount + ", balance: " + getBalance());
+            return true;
+        }
     }
-
 }
--- src/Person.java
+++ src/Person.java
@@ -39,18 +39,31 @@ public class Person {
 
     // 첫 번째 파라미터 : 받는 사람(Person)
     // 두 번째 파라미터 : 이체할 금액(정수)
-//    // 리턴 : 성공여부(불린)
-//    public boolean transfer(Person to, int amount) {
-//        setOwner(to);
-//        return deposit(amount);
-//    }
-//
-//    // 첫 번째 파라미터: 받는 사람의 계정 (BankAccount)
-//    // 두 번째 파라미터: 이체할 금액 (정수)
-//    // 리턴 : 성공여부 (불린)
-//    public boolean transfer(BankAccount to, int amount) {
-//        setOwner(to);
-//        return withdraw(amount);
-//    }
+    // 리턴 : 성공여부(불린)
+    public boolean transfer(Person to, int amount) {
+        if (amount < 0 && amount > getAccount().getBalance()) {
+            System.out.println("false - from: " + getName() + ", to: " + to.getName() + ", amount: " + amount + ", balance: " + getAccount().getBalance());
+            return false;
+        } else {
+            getAccount().setBalance(getAccount().getBalance() - amount);
+            to.getAccount().setBalance(to.getAccount().getBalance() + amount);
+            System.out.println("true - from: " + getName() + ", to: " + to.getName() + ", amount: " + amount + ", balance: " + getAccount().getBalance());
+            return true;
+        }
+    }
 
+    // 첫 번째 파라미터: 받는 사람의 계정 (BankAccount)
+    // 두 번째 파라미터: 이체할 금액 (정수)
+    // 리턴 : 성공여부 (불린)
+    public boolean transfer(BankAccount to, int amount) {
+        if (amount < 0 && amount > getAccount().getBalance()) {
+            System.out.println("false - from: " + getName() + ", to: " + to.getOwner().getName() + ", amount: " + amount + ", balance: " + getAccount().getBalance());
+            return false;
+        } else {
+            getAccount().setBalance(getAccount().getBalance() - amount);
+            to.setBalance(to.getBalance() + amount);
+            System.out.println("true - from: " + getName() + ", to: " + to.getOwner().getName() + ", amount: " + amount + ", balance: " + getAccount().getBalance());
+            return true;
+        }
+    }
 }
