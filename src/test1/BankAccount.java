d2cc60d35935fcfc61ca8cf4e5d88aae9c13d0a7
oocheol <oocheol@naver.com>
2022-01-23 14:38:28 +0900

    22/01/23

--- src/test1/BankAccount.java
+++ src/test1/BankAccount.java
@@ -22,45 +22,6 @@ public class BankAccount {
         return owner;
     }
 
-
-
-    // 파라미터 : 입금할 액수(정수)
-    // 리턴 : 성공여부(불린)
-    boolean deposit(int amount) {
-
-        if (owner.getCashAmount() - amount >= 0){
-            setBalance(getBalance() + amount);
-            int total = owner.getCashAmount() - amount;
-            owner.setCashAmount(total);
-            System.out.println(amount + "원 입금하였습니다. 잔고: "+ getBalance() + "원, 현금: "+ owner.getCashAmount()+"원");
-            return true;
-        }
-        else {
-            System.out.println("입금 실패입니다. 잔고: "+ getBalance() + "원, 현금: "+ owner.getCashAmount()+"원");
-            return false;
-        }
-
-    }
-    public boolean depositWithExchangeRate(double amount, double exchangeRate) {
-        return deposit((int) (amount * exchangeRate));
-    }
-
-    // 파라미터 : 출금할 액수(정수)
-    // 리턴 : 성공여부(불린)
-    boolean withdraw(int amount) {
-        if (getBalance() - amount >= 0){
-            setBalance(getBalance() - amount);
-            int total = owner.getCashAmount() + amount;
-            owner.setCashAmount(total);
-            System.out.println(amount + "원 출금하였습니다. 잔고: "+ getBalance() + "원, 현금: "+ owner.getCashAmount()+"원");
-            return true;
-        }
-        else {
-            System.out.println("출금 실패입니다. 잔고: "+ getBalance() + "원, 현금: "+ owner.getCashAmount()+"원");
-            return false;
-        }
-    }
-
     // 첫 번째 파라미터 : 받는 사람(Person)
     // 두 번째 파라미터 : 이체할 금액(정수)
     // 리턴 : 성공여부(불린)
